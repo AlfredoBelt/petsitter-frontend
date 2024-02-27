30ac8bc5b28868f492d2defb5cba8d375edbe38f
Level <level@MacBook-Pro-de-Level.local>
2024-02-27 16:26:36 -0700

    slider viejo

---
+++ src/modules/apointmeint/index.js
@@ -0,0 +1,23 @@
+import { Button, ButtonGroup, Grid } from "@mui/material";
+
+export default function Apointmeint(){
+    return(
+        <Grid container>
+            <Grid item>
+                <ButtonGroup>
+                    <Button></Button>
+                </ButtonGroup>
+
+            </Grid>
+            <Grid item>
+                
+            </Grid>
+            <Grid item>
+                
+            </Grid>
+            <Grid item>
+                
+            </Grid>
+        </Grid>
+    )
+}
--- src/modules/infopetsitter/slider.js
+++ src/modules/infopetsitter/slider.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function SliderImg(){
     return(
-        <Box sx={{padding:{xs:"0",md:"3em"}}}>
-        <Box sx={{display:"flex", overflow:"auto", columnGap:"16em", background:{xs:"transparent", md:"white"}, padding:"2em", borderRadius:"1em" }}>
+        <Box sx={{padding:{xs:"0",md:"3em"}}} >
+        <Box sx={{display:"flex", overflow:"auto", columnGap:"16em", background:{xs:"transparent", md:"white"}, padding:"2em", borderRadius:"1em" }} >
             <Image src={"/sliderimg_00.svg"} width={500} height={300} style={{width:"100%", height:"auto"}} alt="imagen slider" />
             <Image src={"/sliderimg_01.svg"} width={500} height={300} style={{width:"100%", height:"auto"}} alt="imagen slider" />
             <Image src={"/sliderimg_02.svg"} width={500} height={300} style={{width:"100%", height:"auto"}} alt="imagen slider" />
